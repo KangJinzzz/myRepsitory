bdbc46744efe201eacdc5703a095ed8a2872b299
kangJinzzz <1243363939@qq.com>
2019-10-21 18:59:59 +0800

    提交

--- LibraryManagementSystem/src/operation/DeleteBook.java
+++ LibraryManagementSystem/src/operation/DeleteBook.java
@@ -2,9 +2,21 @@ package operation;
 
 import book.BookList;
 
+import java.util.Scanner;
+
 public class DeleteBook implements IOperation {
     @Override
     public void work(BookList bookList) {
-
+        System.out.println("请输入要删除书籍的名字：");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        int size = bookList.getSize();
+        for (int i = 0; i < size; i++) {
+            if(name.equals(bookList.getBooks()[i].getName())) {
+                bookList.getBooks()[i] = bookList.getBooks()[size - 1];
+                bookList.setSize(size - 1);
+                return;
+            }
+        }
     }
 }
--- LibraryManagementSystem/src/operation/DisplayBook.java
+++ LibraryManagementSystem/src/operation/DisplayBook.java
@@ -5,9 +5,9 @@ import book.BookList;
 public class DisplayBook implements IOperation {
     @Override
     public void work(BookList bookList) {
+        System.out.println("共有" + bookList.getSize() + "本书！");
         for (int i = 0; i < bookList.getSize(); i++) {
             System.out.println(bookList.getBooks()[i].toString());
-
         }
     }
 }
