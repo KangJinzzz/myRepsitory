bc3233845ca5b086401c8147d5958b3b0304feb8
kangJinzzz <1243363939@qq.com>
2019-10-27 19:33:24 +0800

    提交

---
+++ MyString/src/MyString.java
@@ -0,0 +1,36 @@
+
+public class MyString {
+
+//    1. equals
+    public boolean MyEquals(String str1, String str2) {
+        for (int i = 0; i < str1.length(); i++) {
+            if(str.charAt(i) != )
+
+        }
+    }
+//    2. compareTo
+//    3. toCharArray
+    public char[] MyToCharArray(String str) {
+        char[] arrs = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arrs[i] = str.charAt(i);
+        }
+        return arrs;
+    }
+//    4. contains
+//    5. indexOf
+//    6. lastIndexOf
+//    7. replaceAll
+//    8. replaceFirst
+//    9. split
+//    10. subString
+//    11. trim
+//    12. isEmpty
+//    13. length
+//    public int MyLength(String str) {
+//
+//    }
+}
+
+
+
