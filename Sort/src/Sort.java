173099f394c9ea5256a23eb27f6976db1c76f861
kangJinzzz <1243363939@qq.com>
2019-11-20 09:23:08 +0800

    提交

--- Sort/src/Sort.java
+++ Sort/src/Sort.java
@@ -1,3 +1,4 @@
+import javax.swing.tree.TreeNode;
 import java.util.Arrays;
 
 public class Sort {
@@ -43,20 +44,81 @@ public class Sort {
         }
     }
 
+    //选择排序
+    public static void selectSort(int[] array) {
+        // 先创建一个 bound 变量, 表示边界
+        // [0, bound) 已排序区间
+        // [bound, size) 待排序区间
+        for (int bound = 0; bound < array.length; bound++) {
+            // 使用打擂台的方式找到待排序区间中的最小值
+            // bound 位置的元素就是擂台
+            for (int cur = bound + 1; cur < array.length; cur++) {
+                if (array[cur] < array[bound]) {
+                    // 打败擂主, 就和擂主交换
+                    swap(array, cur, bound);
+                }
+            }
+        }
+    }
 
+    //堆排序
+    public static void heapSort(int[] array) {
+        createHeap(array);
+        for (int i = 0; i < array.length - 1; i++) {
+            swap(array, 0, array.length - 1 - i);
+            shiftDown(array, array.length - 1 - i, 0);
+        }
+    }
+    private static void createHeap(int[] array) {
+        for (int i = (array.length - 1) / 2; i >= 0; i--) {
+            shiftDown(array, array.length, i);
+        }
+    }
+    private static void shiftDown(int[] array,int size, int index) {
+        int parent = index;
+        int child = 2 * index + 1;
+        while (child < size) {
+            if(child + 1 < size && array[child + 1] > array[child]) {
+                child++;
+            }
+            if(array[parent] < array[child]) {
+                swap(array, parent, child);
+            } else {
+                break;
+            }
+            parent = child;
+            child = 2 * parent + 1;
+        }
+    }
+
+    //冒泡排序
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = true;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if(array[j] > array[j + 1]) {
+                    swap(array, j, j + 1);
+                    isSorted = false;
+                }
+            }
+            if(isSorted) {
+                break;
+            }
+        }
+    }
     public static void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
 
-
-
-
     public static void main(String[] args) {
         int[] array = {2, 4, 1, 6, 7, 9, 0, 5, 3, 8};
         //insertSort(array);
-        shellSort(array);
+        //shellSort(array);
+        //selectSort(array);
+        //heapSort(array);
+        bubbleSort(array);
         System.out.println(Arrays.toString(array));
     }
 }
