4610922093698dde342d4452c9876d23cd9e10b3
kangJinzzz <1243363939@qq.com>
2019-11-28 18:59:23 +0800

    提交

--- OSExperiment/src/Experiment_1/Scheduling.java
+++ OSExperiment/src/Experiment_1/Scheduling.java
@@ -1,8 +1,6 @@
 package Experiment_1;
-
-
-
 import java.util.*;
+
 
 class Process implements Comparable<Process> {
     public String name = null;
@@ -26,14 +24,6 @@ class Process implements Comparable<Process> {
         this.TRemaining = TService;
     }
 
-    public void setTComplete(int TComplete) {
-        this.TComplete = TComplete;
-    }
-
-    public void setW(double w) {
-        W = w;
-    }
-
     @Override
     public String toString() {
         return "Process{" +
@@ -55,33 +45,52 @@ class Process implements Comparable<Process> {
 public class Scheduling {
     public List<Process> list = new ArrayList<>();
 
-    public void add(String name,int t1, int t2) {
-        list.add(new Process(name, t1, t2));
+    public void add2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入处理进程个数：");
+        int n = scanner.nextInt();
+        while (n > 0) {
+            System.out.println("请输入进程名：");
+            String name = scanner.next();
+            System.out.println("请输入到达时间：");
+
+            int TArrive = scanner.nextInt();
+            System.out.println("请输入服务时间：");
+
+            int TService = scanner.nextInt();
+            list.add(new Process(name, TArrive, TService));
+            n--;
+        }
+        for (Process process : list) {
+            process.isDealed = false;
+        }
     }
 
-    public Process find(String name) {
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).name.equals(name)) {
-                return list.get(i);
+
+
+    //先到先服务
+    public void firstCome() {
+        initialize();
+        Collections.sort(list);
+        int time = 0;
+        Queue<Process> queue = new LinkedList<>(list);
+        while (!queue.isEmpty()) {
+            Process deal = queue.poll();
+            assert deal != null;
+            if(deal.TArrive > time) {
+                time = deal.TArrive;
             }
+            deal.TComplete = time + deal.TService;
+            deal.Ti = deal.TComplete - deal.TArrive ;
+            deal.W = (double)deal.Ti / deal.TService;
+            time = deal.TComplete;
         }
-        return null;
-    }
-    public Process find(int TServiceMin) {
-        for (Process aList : list) {
-            if (aList.TService == TServiceMin && !aList.isDealed) {
-                aList.isDealed = true;
-                return aList;
-            }
-        }
-        return null;
-    }
-    public void remove(String name) {
-        list.remove(find(name));
     }
 
-    //短作业优先
+
+    //短作业优先 SJF
     public void shortService() {
+        initialize();
         Collections.sort(list);
         LinkedList<Process> deal = new LinkedList<>(list);
         int t = 0;
@@ -95,12 +104,20 @@ public class Scheduling {
                     toRemove = goal;
                 }
             }
+
             if (toRemove == null) {
                 toRemove = deal.peekFirst();
                 assert toRemove != null;
                 t = toRemove.TArrive;
+                for (Process goal : deal) {
+                    if (goal.TArrive <= t && TServiceMin >= goal.TService) {
+                        TServiceMin = goal.TService;
+                        toRemove = goal;
+                        t = toRemove.TArrive;
+                    }
+                }
             }
-            Process process = find(TServiceMin);
+            Process process = toRemove;
             process.TComplete = t + TServiceMin;
             process.Ti = process.TComplete - process.TArrive ;
             process.W = (double)process.Ti / process.TService;
@@ -111,8 +128,11 @@ public class Scheduling {
 
     //时间片轮转
     public void timeRotation() {
+        initialize();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入时间片q：");
         //时间片
-        int q = 1;
+        int q = scanner.nextInt();
         int time = 0;
         Collections.sort(list);
         LinkedList<Process> nPerformed = new LinkedList<>(list);
@@ -161,12 +181,7 @@ public class Scheduling {
 
                 queue.offer(nDeal);
             }
-
-
         }
-
-
-
 
         for (Process process : list) {
             process.Ti = process.TComplete - process.TArrive;
@@ -179,7 +194,30 @@ public class Scheduling {
         for (Process aList : list) {
             System.out.println(aList.toString());
         }
+        double sumW = 0;
+        double w = 0;
+        for (Process process : list) {
+            sumW += process.W;
+        }
+        w = sumW / list.size();
+        System.out.println("w = " + w);
     }
 
+    public void menu() {
+        System.out.println("================");
+        System.out.println("请选择功能 ：");
+        System.out.println("1.先到先服务算法");
+        System.out.println("2.短作业优先算法");
+        System.out.println("3.时间片轮转算法");
+        System.out.println("0.退出");
+        System.out.println("================");
+    }
+
+    private void initialize () {
+        for (Process process : list) {
+            process.TRemaining = process.TService;
+            process.isDealed = false;
+        }
+    }
 
 }
--- OSExperiment/src/Experiment_1/Test.java
+++ OSExperiment/src/Experiment_1/Test.java
@@ -1,18 +1,43 @@
 package Experiment_1;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Scheduling sc = new Scheduling();
-        sc.add("A", 0, 4);
-        sc.add("B", 1, 3);
-        sc.add("C", 2, 4);
-        sc.add("D", 3, 2);
-        sc.add("E", 4, 4);
-
-        sc.shortService();
-        //sc.timeRotation();
+        sc.add2();
         sc.display();
 
+        while(true) {
+            sc.menu();
+            int choice = scanner.nextInt();
+            if(choice == 0) {
+                System.exit(0);
+            } else if (choice == 1) {
+                sc.firstCome();
+                sc.display();
+
+            } else if (choice == 2) {
+                sc.shortService();
+                sc.display();
+
+            } else if (choice == 3) {
+                sc.timeRotation();
+                sc.display();
+
+            } else {
+                System.out.println("输入错误！");
+            }
+        }
+
+
+
+
+
     }
+
+
+
 
 }
