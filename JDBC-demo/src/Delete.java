ee283437876fb0087cc4961e31b1be6bc3c11992
kangJinzzz <1243363939@qq.com>
2019-12-08 20:11:19 +0800

    提交

---
+++ JDBC-demo/src/Delete.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Delete {
+
+    public static void main(String[] args) {
+        delete(8);
+    }
+
+    public static void delete(int dropId) {
+        String sql = "delete from exam_result where id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DBUtil.getConnection2();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, dropId);
+
+            int num = statement.executeUpdate();
+            if(num > 0) {
+                System.out.println("删除成功！");
+            } else {
+                System.out.println("删除失败！");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.close(connection, statement,null);
+        }
+    }
+
+
+
+}
