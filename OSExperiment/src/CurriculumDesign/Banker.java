be2a0a55c30607340289571e2434f01825aea81f
kangJinzzz <1243363939@qq.com>
2019-12-15 17:55:40 +0800

    提交

--- OSExperiment/src/CurriculumDesign/Banker.java
+++ OSExperiment/src/CurriculumDesign/Banker.java
@@ -1,8 +1,8 @@
 package CurriculumDesign;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 
@@ -14,6 +14,8 @@ class Process {
     //该进程各类资源尚需的资源数目
     public int[] need = new int[3];
 
+    public boolean finish = false;
+
     public Process() {
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < max.length; i++) {
@@ -21,6 +23,16 @@ class Process {
             max[i] = scanner.nextInt();
             need[i] = max[i];
         }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Process{" +
+                "max=" + Arrays.toString(max) +
+                ", allocation=" + Arrays.toString(allocation) +
+                ", need=" + Arrays.toString(need) +
+                '}';
     }
 }
 
@@ -49,4 +61,29 @@ public class Banker {
     }
 
 
+    public void display() {
+        for (Process process : list) {
+            System.out.println(process.toString());
+        }
+    }
+
+    public void bankerAlgorithm() {
+        //先拷贝一份list，newList
+        List<Process> newList = new ArrayList<Process>();
+        for (Process)
+
+        int[] arr = new int[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("输入进行资源分配的Process：");
+        int n = scanner.nextInt();
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("source[%d]", i);
+            arr[i] = scanner.nextInt();
+        }
+
+        //对newList进行试分配，成功进行安全算法，不成功直接返回
+
+
+
+    }
 }
--- OSExperiment/src/CurriculumDesign/Test.java
+++ OSExperiment/src/CurriculumDesign/Test.java
@@ -3,6 +3,6 @@ package CurriculumDesign;
 public class Test {
     public static void main(String[] args) {
         Banker banker = new Banker();
-
+        banker.display();
     }
 }
