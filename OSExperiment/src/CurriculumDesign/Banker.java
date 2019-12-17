c71cb4ae9ae1f487926b7a634fc4530cd7c4520e
kangJinzzz <1243363939@qq.com>
2019-12-18 07:56:01 +0800

    提交

--- OSExperiment/src/CurriculumDesign/Banker.java
+++ OSExperiment/src/CurriculumDesign/Banker.java
@@ -1,56 +1,63 @@
 package CurriculumDesign;
 
-import java.io.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
-class Process {
-    //该进程对各类资源需要的最大数目
-    public int[] max = new int[3];
-    //该进程各类资源已分配的资源数目
-    public int[] allocation = new int[3];
-    //该进程各类资源尚需的资源数目
-    public int[] need = new int[3];
 
-    public boolean finish = false;
 
-    public Process() {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < max.length; i++) {
-            System.out.printf("max[%d]:", i);
-            max[i] = scanner.nextInt();
-            need[i] = max[i];
+public class Banker {
+    public static int SIZE = 3;
+
+    class Process {
+        //该进程对各类资源需要的最大数目
+        public int[] max = new int[SIZE];
+        //该进程各类资源已分配的资源数目
+        public int[] allocation = new int[SIZE];
+        //该进程各类资源尚需的资源数目
+        public int[] need = new int[SIZE];
+
+        public boolean finish = false;
+
+        public Process() {
+            Scanner scanner = new Scanner(System.in);
+            for (int i = 0; i < max.length; i++) {
+                System.out.printf("allocation[%d]:", i);
+                allocation[i] = scanner.nextInt();
+            }
+            for (int i = 0; i < max.length; i++) {
+                System.out.printf("need[%d]:", i);
+                need[i] = scanner.nextInt();
+            }
+            for (int i = 0; i < max.length; i++) {
+                max[i] = allocation[i] + need[i];
+            }
+        }
+
+        public Process(Process process) {
+            max = Arrays.copyOf(process.max, process.max.length);
+            allocation = Arrays.copyOf(process.allocation, process.allocation.length);
+            need = Arrays.copyOf(process.need, process.need.length);
+
+        }
+
+
+        @Override
+        public String toString() {
+            return "Process{" +
+                    "max=" + Arrays.toString(max) +
+                    ", allocation=" + Arrays.toString(allocation) +
+                    ", need=" + Arrays.toString(need) +
+                    '}';
         }
     }
 
-    public Process(Process process) {
-        max = Arrays.copyOf(process.max, process.max.length);
-        allocation = Arrays.copyOf(process.allocation, process.allocation.length);
-        need = Arrays.copyOf(process.need, process.need.length);
-
-    }
 
 
-    @Override
-    public String toString() {
-        return "Process{" +
-                "max=" + Arrays.toString(max) +
-                ", allocation=" + Arrays.toString(allocation) +
-                ", need=" + Arrays.toString(need) +
-                '}';
-    }
-}
 
-public class Banker {
     public List<Process> list = new ArrayList<Process>();
 
-    public int[] Max = new int[3];
     //可用资源数
-    public int[] Available = new int[3];
+    public int[] Available = new int[SIZE];
 
     public Banker() {
         Scanner scanner = new Scanner(System.in);
@@ -63,15 +70,15 @@ public class Banker {
         }
 
         System.out.println("资源初始化：");
-        for (int i = 0; i < Max.length; i++) {
-            System.out.printf("Max[%d]:", i);
-            Max[i] = scanner.nextInt();
-            Available[i] = Max[i];
+        for (int i = 0; i < Available.length; i++) {
+            System.out.printf("Available[%d]:", i);
+            Available[i] = scanner.nextInt();
         }
     }
 
 
     public void display() {
+        System.out.println(Arrays.toString(Available));
         for (Process process : list) {
             System.out.println(process.toString());
         }
@@ -84,45 +91,104 @@ public class Banker {
             newList.add(new Process(process));
         }
 
-        int[] request = new int[3];
+        int[] request = new int[SIZE];
         Scanner scanner = new Scanner(System.in);
         System.out.print("输入进行资源分配的Process：");
         int n = scanner.nextInt();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < Available.length; i++) {
             System.out.printf("source[%d]", i);
             request[i] = scanner.nextInt();
         }
 
         //对newList进行试分配，成功进行安全算法，不成功直接返回
-        if (request[0] <= Available[0] && request[1] <= Available[1] && request[2] <= Available[2]) {
-            Available[0] -= request[0];
-            Available[1] -= request[1];
-            Available[2] -= request[2];
-            //更新尚需资源
-            newList.get(n).allocation[0] += request[0];
-            newList.get(n).allocation[1] += request[1];
-            newList.get(n).allocation[2] += request[2];
+        boolean flag0 = true;
+        for (int i = 0; i < request.length; i++) {
+            if (request[i] > Available[i]) {
+                flag0 = false;
+                break;
+            }
+        }
+        if (flag0) {
+            for (int i = 0; i < Available.length; i++) {
+                Available[i] -= request[i];
+            }
+
             //更新已分配资源
-            newList.get(n).need[0] -= request[0];
-            newList.get(n).need[1] -= request[1];
-            newList.get(n).need[2] -= request[2];
+            for (int i = 0; i < Available.length; i++) {
+                newList.get(n).allocation[i] += request[i];
+            }
+            //更新尚需资源
+            for (int i = 0; i < Available.length; i++) {
+                newList.get(n).need[i] -= request[i];
+            }
 
         } else {
             System.out.println("分配失败！");
             return;
         }
+        //进行安全检测
         boolean flag = safeCheck(newList);
         if(flag) {
+            for (int i = 0; i < Available.length; i++) {
+                list.get(n).allocation[i] += request[i];
+                list.get(n).need[i] -= request[i];
+            }
             System.out.println("分配成功！");
         } else {
-            System.out.println("没有通过安全性算法， 分配失败！");
+            for (int i = 0; i < Available.length; i++) {
+                Available[i] += request[i];
+            }
         }
     }
 
     public boolean safeCheck(List<Process> newList) {
-        
+        int[] work = new int[SIZE];
+        work = Arrays.copyOf(Available, Available.length);
+        while (true) {
+            Process process = find(newList, work);
+            if(process != null) {
+                for (int i = 0; i < work.length; i++) {
+                    work[i] += process.allocation[i];
+                }
+                process.finish = true;
+            } else {
+                break;
+            }
+        }
+        boolean flag = true;
+        for (Process process : newList) {
+            if(!process.finish) {
+                flag = false;
+                break;
+            }
+        }
+        for (Process process : newList) {
+            process.finish = false;
+        }
+        if (!flag) {
+            System.out.println("没有通过安全性算法， 分配失败！");
+        } else {
+            System.out.println("安全检测通过！");
+        }
+        return flag;
     }
 
+    private Process find(List<Process> newList, int[] work) {
+        for (int j = 0; j < newList.size(); j++) {
+            Process process = newList.get(j);
+            boolean flag = true;
+            for (int i = 0; i < work.length; i++) {
+                if (process.need[i] > work[i]) {
+                    flag = false;
+                }
+            }
+            if (flag && !process.finish) {
+                System.out.printf("P[%d] ", j);
+                return process;
+            }
 
+        }
+        return null;
+    }
 
 }
--- OSExperiment/src/CurriculumDesign/Test.java
+++ OSExperiment/src/CurriculumDesign/Test.java
@@ -1,9 +1,38 @@
 package CurriculumDesign;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Banker banker = new Banker();
         banker.display();
-        banker.bankerAlgorithm();
+
+        while (true) {
+            menu();
+            System.out.println("选择功能");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                banker.bankerAlgorithm();
+                banker.display();
+            } else if (choice == 2) {
+                banker.safeCheck(banker.list);
+            } else if (choice == 0){
+                System.exit(0);
+            } else {
+                System.out.println("输入错误, 重新输入！");
+            }
+        }
+    }
+
+    public static void menu() {
+        System.out.println();
+        System.out.println("==========");
+        System.out.println("1.资源分配");
+        System.out.println("2.安全检测");
+        System.out.println("0.exit");
+        System.out.println("==========");
+
     }
 }
