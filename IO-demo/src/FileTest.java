731df55564fbbeefa08032251b50269a9fa2dd34
kangJinzzz <1243363939@qq.com>
2020-01-07 14:47:21 +0800

    提交

---
+++ IO-demo/src/FileTest.java
@@ -0,0 +1,13 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class FileTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println("sdad");
+    }
+}
